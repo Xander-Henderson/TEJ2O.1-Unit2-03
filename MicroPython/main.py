34f34efb69d01074255d2efe77d2c7761876e7d1
Xander-Henderson <xander.henderson@stu.ocsb.ca>
2025-09-18 16:57:45 +0000

    changed comment

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,33 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Alexander
+Created on: Sep 2025
+This module is a Micro:bit MicroPython program that shows area and perimeter.
 """
 
 from microbit import *
 
+display.clear()
+sleep(1)
 
-display.scroll("Hello, World!")
+"""
+dimensions
+"""
+display.scroll("A rectangle has the of dimensions 5 cm & 3 cm.")
+sleep(0.5)
+display.clear()
+
+"""
+area
+"""
+display.scroll("The area would be: " + str (5 * 3) + "cm^2")
+display.show(Image.HAPPY)
+sleep(0.5)
+display.clear()
+
+"""
+area
+"""
+display.scroll("The perimeter would be: " + str (2 * (5 + 3)) + "cm")
+display.show(Image.HAPPY)
+sleep(0.5)
+display.clear()
