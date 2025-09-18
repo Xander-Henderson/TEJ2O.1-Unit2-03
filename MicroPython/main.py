f28a50905665d1f304b64287ff3222216ceb4e08
Xander-Henderson <xander.henderson@stu.ocsb.ca>
2025-09-18 17:16:20 +0000

    changed comment

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -9,24 +9,18 @@ from microbit import *
 display.clear()
 sleep(1)
 
-"""
-dimensions
-"""
+# dimensions
 display.scroll("A rectangle has the of dimensions 5 cm & 3 cm.")
 sleep(0.5)
 display.clear()
 
-"""
-area
-"""
+#area
 display.scroll("The area would be: " + str (5 * 3) + "cm^2")
 display.show(Image.HAPPY)
 sleep(0.5)
 display.clear()
 
-"""
-area
-"""
+#perimeter
 display.scroll("The perimeter would be: " + str (2 * (5 + 3)) + "cm")
 display.show(Image.HAPPY)
 sleep(0.5)
